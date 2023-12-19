3941f7490767aabc5da1cca5e8d7433b1b63c32c
manish785 <kumarachilish1997@gmail.com>
2023-12-19 20:00:13 +0530

    feat : did firebase setup, deployed app to production, created sign-up user account, implement sign user api and created redux store with userSlice

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,60 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from "./Header";
+import { checkValidData } from '../utils/validate';
+import { auth } from "../utils/firebase";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true); 
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const email = useRef(null);
+    const password = useRef(null);
      
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
+    }
+
+    const handleButtonClick = () => {
+        // Vaildate the form data
+
+        const message = checkValidData(email.current.value, password.current.value);
+        setErrorMessage(message);
+        if(message){
+            return;
+        }
+
+        
+    if (!isSignInForm) {
+        // Sign Up Logic
+        createUserWithEmailAndPassword(
+          auth,
+          email.current.value,
+          password.current.value
+        )
+          .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode + "-" + errorMessage);
+          });
+      }else{
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+      
+      }
     }
 
     return (
@@ -17,7 +66,9 @@ const Login = () => {
                    alt = 'logo' 
                 />
             </div>
-            <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
+            <form 
+                onSubmit={(e) => e.preventDefault()} 
+                className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white">
                 <h1 className="font-bold text-3xl py-4">
                     {isSignInForm ? 'Sign In' : 'Sign Up'}
                 </h1>
@@ -28,15 +79,18 @@ const Login = () => {
                 />)}
                 <input 
                    type="text" 
+                   ref={email}
                    placeholder="Email Address" 
                    className="p-4 my-4 w-full bg-gray-700"
                 />
                 <input 
                    type="password" 
+                   ref={password}
                    placeholder="Password" 
                    className="p-4 my-4 w-full bg-gray-700"  
                 />
-                <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+                <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
+                <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
                    {isSignInForm ? 'Sign In' : 'Sign Up'}
                 </button>
                 <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
